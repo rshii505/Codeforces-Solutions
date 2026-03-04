e5c0163f0b3d408f6c04875f4e352f528f1e24a0
rshii505 <prshii505@gmail.com>
2026-03-04 12:53:18 +0530

    New Problem - 4/3/2026

---
+++ Problemsets/Java/800/Solution_1999A_A_Plus_B_Again.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Solution_1999A_A_Plus_B_Again {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte t = sc.nextByte();
+        while(t-- != 0)
+        {
+            byte n = sc.nextByte();
+            System.out.println((n/10)+(n%10));
+        }
+    }
+}
