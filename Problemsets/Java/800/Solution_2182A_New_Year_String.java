6e0169f94f11b51007793f8f7366887dec860e26
rshii505 <prshii505@gmail.com>
2026-03-06 21:45:06 +0530

    New Problems - 7/3/2026

---
+++ Problemsets/Java/800/Solution_2182A_New_Year_String.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Solution_2182A_New_Year_String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short t = sc.nextShort();
+        while(t-- != 0)
+        {
+            sc.nextByte();
+            String s = sc.next();
+            System.out.println((s.indexOf("2025") == -1) || (s.indexOf("2026") != -1) ? 0 : 1);
+        }
+    }
+}
---
+++ Problemsets/Java/800/Solution_2189A_Table_with_Numbers.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Solution_2189A_Table_with_Numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short t = sc.nextShort();
+        while(t-- != 0)
+        {
+            byte n = sc.nextByte();
+            short h = sc.nextShort();
+            short l = sc.nextShort();
+            if(h > l)
+            {
+                h = (short)(h + l);
+                l = (short)(h - l);
+                h = (short)(h - l);
+            }
+            byte small = 0;
+            byte big = 0;
+            for(byte i = 0; i < n; ++i)
+            {
+                short temp = sc.nextShort();
+                if(temp <= h)
+                    ++small;
+                else if(temp <= l)
+                    ++big;
+            }
+            System.out.println(big > small ? small : big + ((small-big)/2));
+        }
+    }
+}
