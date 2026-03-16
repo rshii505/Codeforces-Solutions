fbef8f9025c6649f41914eba4636e914b0edb7e0
rishi <prshii505@gmail.com>
2026-03-16 23:28:37 +0530

    New Problems - 16/3/2026

---
+++ Problemsets/Java/800/Solution_2044A_Easy_Problem.java
@@ -0,0 +1,10 @@
+// https://codeforces.com/problemset/problem/2044/A
+import java.util.Scanner;
+public class Solution_2044A_Easy_Problem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte t = sc.nextByte();
+        while(t-- != 0)
+            System.out.println(sc.nextByte() - 1);
+    }
+}
