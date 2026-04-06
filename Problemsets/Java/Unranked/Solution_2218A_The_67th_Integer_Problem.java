79e9aa1fe30d0a1b14dd3c43cdd9787195fc171e
rishi <prshii505@gmail.com>
2026-04-06 22:56:31 +0530

    New Problems - 6/4/2026

---
+++ Problemsets/Java/Unranked/Solution_2218A_The_67th_Integer_Problem.java
@@ -0,0 +1,13 @@
+// https://codeforces.com/problemset/problem/2218/A
+import java.util.Scanner;
+public class Solution_2218A_The_67th_Integer_Problem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        short t = sc.nextShort();
+        while(t-- != 0)
+        {
+            sc.nextByte();
+            System.out.println(67);
+        }
+    }
+}
