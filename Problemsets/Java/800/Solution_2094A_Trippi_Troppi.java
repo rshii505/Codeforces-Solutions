69e890246986f2d439c2d8b1d8119139b0b8ef25
rshii505 <prshii505@gmail.com>
2026-04-23 17:19:22 +0530

    New Problems - 23/4/2026

---
+++ Problemsets/Java/800/Solution_2094A_Trippi_Troppi.java
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/2094/A
+import java.util.Scanner;
+public class Solution_2094A_Trippi_Troppi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte t = sc.nextByte();
+        while(t-- != 0)
+          System.out.println("" + sc.next().charAt(0) + sc.next().charAt(0) + sc.next().charAt(0));
+        
+    }
+}
