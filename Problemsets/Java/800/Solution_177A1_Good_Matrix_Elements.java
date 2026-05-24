47f2bdd15815c660cc37638f6afc6e4398178dfc
rishi <prshii505@gmail.com>
2026-05-24 23:32:29 +0530

    New Problems - 24/5/2026

---
+++ Problemsets/Java/800/Solution_177A1_Good_Matrix_Elements.java
@@ -0,0 +1,25 @@
+// https://codeforces.com/problemset/problem/177/A1
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Solution_177A1_Good_Matrix_Elements {
+    public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      byte n =  Byte.parseByte(br.readLine());
+      short sum = 0;
+      byte mid = (byte)((n + 1) / 2);
+      for(byte i = 1; i <= n; ++i)
+      {
+        StringTokenizer inpt = new StringTokenizer(br.readLine());
+        for(byte j = 1; j <= n; ++j)
+        {
+          if((i == j) || (i + j == n + 1) || (i == mid) || (j == mid))
+            sum += Short.parseShort(inpt.nextToken());
+          else 
+            inpt.nextToken();
+        }
+      }
+      System.out.println(sum);
+    }
+}
